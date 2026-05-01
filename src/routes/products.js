fde0e817f2e400f92ed6697652b4d32b5fc757b9
Phillip Abernathy <moraff@gmail.com>
2026-05-01 05:12:03 -0400

    skeleton for products endpoint

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Get all products' });
+  res.json({ message: 'Get all products.' });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({message: `Item ${id} retrieved.`});
+});
+
+router.post('/', (req, res) => {
+  res.status(201).json({message: 'Item created'});
+});
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ message: `Item ${id} updated`});
+});
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ message: `Item ${id} deleted.`});
 });
 
 module.exports = router;
