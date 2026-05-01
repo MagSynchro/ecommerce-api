e2daa36125967653ca18bb97e15abdfbd5b44a9d
Phillip Abernathy <moraff@gmail.com>
2026-05-01 02:10:54 -0400

    Wired up routes.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+const userRoutes = require('./routes/users');
+const productRoutes = require('./routes/products');
+const cartRoutes = require('./routes/carts');
+const orderRoutes = require('./routes/orders');
+
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/cart', cartRoutes);
+app.use('/orders', orderRoutes);
+
 app.get('/', (req, res) => {
   res.json({ message: 'API is running' });
 });
