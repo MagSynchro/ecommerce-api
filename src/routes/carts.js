8fdfb8c83951789615229f1c316955512bc595af
Phillip Abernathy <moraff@gmail.com>
2026-05-03 02:25:10 -0400

    completed carts.js routes and connected to user validation.

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -13,10 +13,6 @@ router.use(ensureAuthenticated);
 
 router.get('/', async (req, res) => {
   try {
-    if (!req.user){
-      return res.status(401).json({ message: 'Not logged in'});
-    }
-  
     const userId = req.user.id;
     const result = await pool.query(
       `SELECT c.id, p.name, p.price, c.quantity
@@ -32,16 +28,74 @@ router.get('/', async (req, res) => {
   }
   });
 
-router.post('/', (req, res) => {
-  res.status(201).json({message: 'Product added to cart.'});
+router.post('/', async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { product_id, quantity } = req.body;
+
+    const result = await pool.query(
+      `INSERT INTO cart_items (user_id, product_id, quantity)
+       VALUES ($1, $2, $3)
+       RETURNING *`,
+      [userId, product_id, quantity]
+    );
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
-router.put('/:id', (req, res) => {
+
+router.put('/:id', async (req, res) => {
+  const userId = req.user.id;
   const { id } = req.params;
-  res.json({ message: `Product ${id} updated in cart.`});
+  const { quantity } = req.body;
+
+  try {
+    const result = await pool.query(
+      `UPDATE cart_items
+      SET quantity = $1
+      WHERE id = $2 AND user_id = $3
+      RETURNING *`,
+      [quantity, id, userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'Cart item not found' });
+    }
+
+    res.json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
+  const userId = req.user.id;
   const { id } = req.params;
-  res.json({ message: `Product ${id} removed from cart.`});
+  try {
+  const result = await pool.query(
+    `DELETE FROM cart_items
+     WHERE id = $1 AND user_id = $2
+     RETURNING *`,
+    [id, userId]
+  );
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ message: 'Cart item not found' });
+  }
+
+  res.sendStatus(204);
+
+} catch (err) {
+  console.error(err);
+  res.status(500).json({ message: 'Server error' });
+}
+  
 });
 
 module.exports = router;
