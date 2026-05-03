5c18f890a6e81554369be20dd899062b3b37d73b
Phillip Abernathy <moraff@gmail.com>
2026-05-03 00:46:26 -0400

    added login to route.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const userController = require('../controllers/usersController');
+const passport = require('passport');
 const router = express.Router();
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 
-router.post('/login', (req, res) => {
-  res.status(201).json({message: 'User Login'});
-});
+router.post('/login',  passport.authenticate('local'),
+  (req, res) => {
+    res.json({
+      message: 'Login successful',
+      user: req.user
+    });
+  }
+);
 router.post('/register', userController.createUser);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.removeUser);
