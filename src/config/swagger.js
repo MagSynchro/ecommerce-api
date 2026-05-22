1d0f03608093145af19b33ebbc43006127758a2f
Phillip Abernathy <moraff@gmail.com>
2026-05-22 06:48:04 -0400

    Adding Swagger Schema.

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -13,8 +13,67 @@ const options = {
         url: 'http://localhost:3000',
       },
     ],
+
+
+    components: {
+      schemas: {
+        User: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'integer'
+            },
+            email: {
+              type: 'string'
+            }
+          }
+        },
+
+        Product: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            user_id: { type: 'integer' },
+            product_id: { type: 'integer' },
+            quantity: { type: 'integer' }
+          }
+        },
+
+        CartItem: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            product_id: { type: 'integer' },
+            quantity: { type: 'integer' }
+          }
+        },
+
+        Order: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            user_id: { type: 'integer' },
+            created_at: { type: 'string', format: 'date-time' }
+          }
+        },
+
+        OrderItem: {
+          type: 'object',
+          properties: {
+            id: { type: 'integer' },
+            order_id: { type: 'integer' },
+            product_id: { type: 'integer' },
+            quantity: { type: 'integer' },
+            price_at_time: { type: 'number' }
+          }
+        }
+
+
+      }
+    }
   },
-  apis: ['./src/routes/*.js'], // where your route comments will live
+
+  apis: ['./src/routes/*.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
