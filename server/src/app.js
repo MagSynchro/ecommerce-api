2744cca68eb2720a1afe859e7dbf779165277eec
Phillip Abernathy <moraff@gmail.com>
2026-05-23 02:50:54 -0400

    CORS implementation

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,17 @@ const session = require('express-session');
 const passport = require('passport');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
+const cors = require("cors");
 require('./config/passport');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.FRONT_END_SERVER,
+  credentials: true
+}));
 
 app.use(session({
   secret: 'dev_secret',
