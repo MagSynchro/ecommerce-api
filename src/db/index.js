9cb096b23f71acf4d1299a7e473f7d8ad924a594
Phillip Abernathy <moraff@gmail.com>
2026-05-23 00:54:47 -0400

    final commit. changed to env files and rotated db port and credentials

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ecommerce-api-project',
-  password: 'N1nt3nd0!',
-  port: 6644,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT || 5432,
 });
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const pool = require('./db');
+require('dotenv').config();
 
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
@@ -9,7 +10,7 @@ pool.query('SELECT NOW()', (err, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
